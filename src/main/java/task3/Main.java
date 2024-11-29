b5347f7aad7c78ecdd190cac7bfb42c578e104dd
andrewharlov <dronindigo@gmail.com>
2024-11-29 18:07:39 +0300

    Did minor refactoring.

--- src/main/java/task3/Main.java
+++ src/main/java/task3/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class task3 {
+public class Main {
     public static void main(String[] args){
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9,10);
 
